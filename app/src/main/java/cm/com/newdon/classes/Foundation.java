f20271a9e6dc0580b7e08cf8c28b5b1d76f70148
Marina <rappoportmarina@gmail.com>
2016-12-17 19:16:10 +0200

    added FoundCategory class, filled Foundation class

--- app/src/main/java/cm/com/newdon/classes/Foundation.java
+++ app/src/main/java/cm/com/newdon/classes/Foundation.java
@@ -1,8 +1,37 @@
 package cm.com.newdon.classes;
 
+import com.facebook.FacebookRequestError;
+
+import java.net.URL;
+
 /**
  * Class represents foundation
  */
 
 public class Foundation {
+
+    private int id;
+    private String title;
+    private String description;
+    private String nubmer;
+    private int yearFounded;
+    private String adress;
+
+//    ???
+    private String logoUrl;
+
+    private int donatorCount;
+    private FoundCategory category;
+
+    public Foundation(int id, String title, FoundCategory category, String adress, String logoUrl) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.adress = adress;
+        this.logoUrl = logoUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
