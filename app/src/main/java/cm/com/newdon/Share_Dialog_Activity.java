b90cf9d6864c015f385380bf4fb43b0df285c3a0
Marina <rappoportmarina@gmail.com>
2017-01-21 22:09:31 +0200

    added graphic for notification and for share dialog

--- app/src/main/java/cm/com/newdon/Share_Dialog_Activity.java
+++ app/src/main/java/cm/com/newdon/Share_Dialog_Activity.java
@@ -2,6 +2,7 @@ package cm.com.newdon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Share_Dialog_Activity extends Activity {
 
@@ -9,5 +10,9 @@ public class Share_Dialog_Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share__dialog_);
+    }
+
+    public void closeShareDialog(View view) {
+        finish();
     }
 }
