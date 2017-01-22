0c2c31d40ee782ff2252b944c0734d4442f30550
Marina <rappoportmarina@gmail.com>
2017-01-22 21:33:07 +0200

    refactor name of activity

--- app/src/main/java/cm/com/newdon/ShareDialogActivity.java
+++ app/src/main/java/cm/com/newdon/ShareDialogActivity.java
@@ -4,12 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Share_Dialog_Activity extends Activity {
+public class ShareDialogActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_share__dialog_);
+        setContentView(R.layout.activity_share_dialog);
     }
 
     public void closeShareDialog(View view) {
