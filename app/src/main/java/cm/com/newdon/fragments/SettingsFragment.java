dff0813a4cb86351bcfe3d268b0b6820b69cbe79
costamirkin <costa.mirkin@gmail.com>
2017-04-23 23:40:56 +0300

    Bug fixes

--- app/src/main/java/cm/com/newdon/fragments/SettingsFragment.java
+++ app/src/main/java/cm/com/newdon/fragments/SettingsFragment.java
@@ -32,7 +32,7 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.fragment_settings,container,false);
+        View v =inflater.inflate(R.layout.fragment_settings_new,container,false);
 
 
         supportBg = (ImageView) v.findViewById(R.id.supportBg);
